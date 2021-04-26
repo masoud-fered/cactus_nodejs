cdc9ac43f0321a081b1aa673a196ec1b683775a3
Masoud <masoud.fered@gmail.com>
2021-04-26 23:06:23 +0430

    add handler for register and login

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const router = require('./router');
 const requestHandler = require('./requestHandler');
 
 const handle = {};
-handle['/api/v1/coffee'] = {
+handle['/api/v1/coffees'] = {
     'POST': requestHandler.createCoffee,
     'GET': requestHandler.readCoffee,
     'PUT': requestHandler.updateCoffee,
     'DELETE': requestHandler.deleteCoffee
+}
+handle['/api/v1/users/register'] = {
+    'POST': requestHandler.registerUser
+}
+handle['/api/v1/users/login'] = {
+    'POST': requestHandler.loginUser
 }
 
 server.run(router.route, handle)
